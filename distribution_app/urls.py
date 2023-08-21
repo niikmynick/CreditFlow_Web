e97ec9b14064a4d19ffa1143016341b3e43d987b
niik <fk724s@mail.ru>
2023-08-21 19:02:36 +0300

    link style changes

--- distribution_app/urls.py
+++ distribution_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import get_info, get_creditors_info, show_results, about
 
 urlpatterns = [
-    path('', get_info),
+    path('', get_info, name='index'),
     path('about', about, name='about'),
     path('creditors_info', get_creditors_info, name='creditors_info'),
     path('results', show_results, name='results'),
