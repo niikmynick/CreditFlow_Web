6f97987cdb1720cd097ed17445f879aa51725ae7
niik <fk724s@mail.ru>
2023-08-21 19:50:23 +0300

    vers1

--- CreditFlow_Web/settings.py
+++ CreditFlow_Web/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'CreditFlow_Web.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
